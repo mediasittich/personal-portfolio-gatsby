54e643604d651f34bb6bfe69edc5f5607dbd2749
Regina Galambos <gina.galambos@gmail.com>
2020-08-22 13:33:20 +0200

    add layout component & dummy data

---
+++ src/constants/data.js
@@ -0,0 +1,8 @@
+export const DATA = [
+  { id: 1, title: "item 1", tags: ["eat"] },
+  { id: 2, title: "item 2", tags: ["sleep"] },
+  { id: 3, title: "item 3", tags: ["work"] },
+  { id: 4, title: "item 4", tags: ["eat", "sleep"] },
+  { id: 5, title: "item 5", tags: ["eat", "work"] },
+  { id: 6, title: "item 6", tags: ["sleep", "work"] },
+]
