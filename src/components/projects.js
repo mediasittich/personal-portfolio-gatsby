cc72f7bbd5dcf26854b132366193c6a99e65b005
Regina Galambos <gina.galambos@gmail.com>
2020-08-22 13:46:29 +0200

    install bootstrap setup projects listing component

---
+++ src/components/projects.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Projects = () => {
+  return <section>List of Projects</section>
+}
+
+export default Projects
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
+import ProjectsList from "../components/projects"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Hello world!</h1>
+      <ProjectsList />
     </Layout>
   )
 }
