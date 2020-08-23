9b45fae73136175f7b1d8b3630a699fd0c3406a1
Regina Galambos <gina.galambos@gmail.com>
2020-08-23 16:14:23 +0200

    add env variables & update API_URL for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,25 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
 
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.API_URL || `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`project`, `category`],
       },
