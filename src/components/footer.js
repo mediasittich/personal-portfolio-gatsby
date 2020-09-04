b01584af99202ae3db6effe4856a2d75cd38f4e5
Regina Galambos <gina.galambos@gmail.com>
2020-09-04 03:10:33 +0200

    add cta to footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,66 @@
 import React from "react"
+import styled from "styled-components"
+import SocialLinks from "../constants/socialLinks"
 
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
+      <FooterCTA>
         <div>
-          <div className="site-logo text-center py-4"></div>
-          <div className="social text-center">
-            <h5 className="text-uppercase">Follow me</h5>
-            <a href="#">icon</a>
-          </div>
-          <div className="copyrights text-center">
-            <p className="para">
-              Copyright ©2019 All rights reserved | This template is made with
-              by
-              <a href="#">
-                <span>Regina Galambos</span>
-              </a>
-            </p>
-          </div>
+          <h1>Wanna work on a project together?</h1>
+          <h5 className="text-uppercase">Get in touch</h5>
+          <SocialLinks />
         </div>
-      </div>
+      </FooterCTA>
+      <FooterLegal>
+        <div className="copyrights text-center">
+          <p className="para">Copyright © 2020 | Imprint | GDPR</p>
+        </div>
+      </FooterLegal>
     </footer>
   )
 }
+
+const FooterCTA = styled.div`
+  width: calc(100% - 90px);
+  margin: 120px auto 0;
+  margin-top: 90px;
+  padding: 25px;
+
+  & > div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 50px 40px;
+
+    & > h1 {
+      font-family: ${props => props.theme.fonts.main};
+      /* font-size: 21px; */
+      margin-bottom: 40px;
+      font-weight: 700;
+    }
+
+    & > h5 {
+      margin-bottom: 30px;
+    }
+
+    & > ul {
+      margin-left: 20px;
+    }
+  }
+`
+
+const FooterLegal = styled.div`
+  position: relative;
+  font-size: 15px;
+  text-align: center;
+  overflow: hidden;
+  padding: 30px 15px;
+  margin-top: 90px;
+  border-top: 1px solid ${props => props.theme.colors.grayBtnLg};
+`
 
 export default Footer
