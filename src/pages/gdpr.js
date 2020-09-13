5d11057c3a8cc3da7ee184bc879666e15a90b4f3
Regina Galambos <gina.galambos@gmail.com>
2020-09-14 00:04:38 +0200

    add initial content to legal pages

---
+++ src/pages/gdpr.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const GDPRPage = () => {
+  return (
+    <Layout>
+      <h1>GDPR</h1>
+    </Layout>
+  )
+}
+
+export default GDPRPage
---
+++ src/pages/imprint.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ImprintPage = () => {
+  return (
+    <Layout>
+      <h1>Imprint</h1>
+    </Layout>
+  )
+}
+
+export default ImprintPage
